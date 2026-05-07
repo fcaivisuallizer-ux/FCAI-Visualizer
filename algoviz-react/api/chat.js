4467671fcf8c993afdbe8e89733984cab3ebad74
birdbox219 <mahmodelsayed882@gmail.com>
2026-05-07 18:12:35 +0300

    fsd

--- algoviz-react/api/chat.js
+++ algoviz-react/api/chat.js
@@ -29,10 +29,11 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-    
+
     if (!response.ok) {
       return res.status(response.status).json(data);
     }
+
 
     return res.status(200).json(data);
   } catch (error) {
